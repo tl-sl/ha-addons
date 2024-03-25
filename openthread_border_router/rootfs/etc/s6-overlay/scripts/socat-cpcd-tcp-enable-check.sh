7211c3be600d1fc8ef2c7ee70c756a8ea595761e
Tim Lunn <tl@smlight.tech>
2024-03-25 19:17:23 +1100

    Fix socat path

--- openthread_border_router/rootfs/etc/s6-overlay/scripts/socat-cpcd-tcp-enable-check.sh
+++ openthread_border_router/rootfs/etc/s6-overlay/scripts/socat-cpcd-tcp-enable-check.sh
@@ -5,6 +5,6 @@
 
 if bashio::config.has_value 'network_device'; then
     touch /etc/s6-overlay/s6-rc.d/user/contents.d/socat-cpcd-tcp
-    touch /etc/s6-overlay/s6-rc.d/cpcd/dependencies.d/socat-cpcd-tcp
+    touch /etc/s6-overlay/s6-rc.d/otbr-agent/dependencies.d/socat-cpcd-tcp
     bashio::log.info "Enabled socat-cpcd-tcp."
 fi
